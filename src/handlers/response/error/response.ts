504be739c597e52c4635fc971311465b2a63ab11
Abdou-Raouf ATARMLA <achilleatarmla@gmail.com>
2024-11-20 10:05:59 +0000

    update: handlers added in order to handle errors directly in the repo framework

---
+++ src/handlers/response/error/response.ts
@@ -0,0 +1,37 @@
+import { ErrorCodes } from "./static";
+import { ErrorCode } from "./types";
+
+export class ErrorResponse extends Error {
+  public statusCode: number;
+  public code: string;
+  public suggestions: string[];
+  public originalError?: Error;
+
+  constructor(
+    code: string,
+    message?: string,
+    suggestions: string[] = [],
+    originalError?: Error,
+  ) {
+    const errorCode: ErrorCode = ErrorCodes[code] || ErrorCodes.GENERAL_ERROR;
+    super(message || errorCode.message);
+    this.code = errorCode.code;
+    this.statusCode = errorCode.statusCode;
+    this.suggestions = suggestions;
+    this.originalError = originalError;
+
+    // Capture the stack trace of the original error if provided
+    if (originalError) {
+      this.stack = originalError.stack;
+    }
+  }
+
+  public toJSON() {
+    return {
+      code: this.code,
+      message: this.message,
+      statusCode: this.statusCode,
+      suggestions: this.suggestions,
+    };
+  }
+}
---
+++ src/handlers/response/types/service-response.ts
@@ -0,0 +1,25 @@
+import { ErrorResponse } from '../error/response';
+
+export type SuccessResponseType<T> = {
+  success: true;
+  meta?: MetaType;
+  data?: {
+    docs?: T | T[];
+    [key: string]: any;
+  };
+};
+
+export type ErrorResponseType = {
+  success: boolean;
+  error: ErrorResponse;
+};
+
+type MetaType = {
+  total?: number;
+  results?: number;
+  page?: number;
+  limit?: number;
+  totalPages?: number;
+  remainingItems?: number;
+  [key: string]: any;
+};
