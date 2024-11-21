499809123cb7c9fb35fe06a4b3a5cb535795f172
Abdou-Raouf ATARMLA <achilleatarmla@gmail.com>
2024-11-21 10:21:23 +0000

    feat: parse config added

---
+++ src/utils/params.ts
@@ -0,0 +1,17 @@
+type SortType = Record<string, 1 | -1>;
+
+export const parseSortParam = (sortParam: string): SortType => {
+  const sortFields = sortParam.split(',');
+  const sortObj: SortType = {};
+
+  sortFields.forEach((field) => {
+    if (field.startsWith('-')) {
+      const fieldName = field.substring(1);
+      sortObj[fieldName] = -1;
+    } else {
+      sortObj[field] = 1;
+    }
+  });
+
+  return sortObj;
+};
